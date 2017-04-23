57ecfd5cb76b2ee5cc0c99506069d2ae6fd21d93
A-gambit <a.gambit.gregory@gmail.com>
2017-04-23 19:29:13 +0300

    add contribution guid

--- packages/big-table-test/index.js
+++ packages/big-table-test/index.js
@@ -2,13 +2,10 @@ window.benchMark = function benchMark() {
   const startTime = performance.now();
   document.querySelector('input').click();
 
-  const endTime = performance.now();
-  const syncTime = endTime - startTime;
-
   setTimeout(() => {
     setTimeout(() => {
       const endTime = performance.now();
-      console.log(`Benchmark time: ${syncTime}, ${endTime - startTime}`);
+      console.log(`Benchmark time: ${endTime - startTime}`);
     }, 0);
   }, 0);
 }
