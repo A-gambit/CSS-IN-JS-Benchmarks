594e8822504e2bb76d89b104a1d5ca81c5b35c17
A-gambit <a.gambit.gregory@gmail.com>
2017-06-21 20:01:10 +0300

    fix

--- packages/benchmarks/styled-components/v1/inline-style/client/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from 'benchmarks-utils';
-import Table from './Table';
-
-import './index.html';
-
-ReactDOM.render(<App table={Table} />, document.getElementById('root'));
